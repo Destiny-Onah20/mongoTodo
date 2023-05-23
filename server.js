2771bf686891bfccda4d97ba2f078d399bf0694e
Destiny <onahdestinyc20@gmail.com>
2023-05-23 09:48:01 +0100

    nest try

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 dotenv.config({path: "./config/config.env"});
 const app = require("./app");
-const port = process.env.PORT
-const databaseUrl = process.env.DATABASE;
+const port = 1800;
+const databaseUrl = "mongodb+srv://onahdestinyc20:4ggoPMQWogMml4kn@cluster0.y9wmotd.mongodb.net/?retryWrites=true&w=majority";
 
 mongoose.connect(databaseUrl, {
   useUnifiedTopology : true,
